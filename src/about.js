fc608c784dab2b5d0ee3853680eb8488c5b8f273
Yardley Gutierrez <yardleyg9@gmail.com>
2021-12-01 22:47:44 -0600

    Fixed more links

--- src/about.js
+++ src/about.js
@@ -1,15 +1,21 @@
 let xbutton = null;
 let xbutton_sub = null;
 let homeb = null;
+let projects = null;
 
 function init(){
     xbutton = document.getElementById("xbutton");
     xbutton_sub = document.getElementById("xbutton-sub");
     homeb = document.getElementById("homeb");
+    projects = document.getElementById("my-projects");
 
 
     homeb.addEventListener("click", function(event){
         document.location.href = "/portfolio-site-new/index.html";
+    });
+
+    projects.addEventListener("click", function(event){
+        document.location.href = "https://github.com/ArkamtheBard";
     });
 
     xbutton.addEventListener("click", function(event) {
